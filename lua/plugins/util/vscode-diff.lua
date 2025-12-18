2d197278eca458aa9e3eac0a6d09cdc7f5dba414
Alex Bergdahl <bergdahlalex@protonmail.com>
2025-12-18 10:34:08 +0100

    vscode-diff added

--- lua/plugins/util/vscode-diff.lua
+++ lua/plugins/util/vscode-diff.lua
@@ -3,6 +3,9 @@ return {
   for_cat = 'general.extra',
   event = "DeferredUIEnter",
   cmd = { "CodeDiff" },
+  keys = {
+    { "<leader>gd", '<cmd>CodeDiff<CR>', desc = "[G]it [D]iff" },
+  },
   -- keys = {
   --   { "<leader>g", mode = { "n" }, function() require("krust").render() end, desc = "Show Rust diagnostics with krust" },
   -- },
